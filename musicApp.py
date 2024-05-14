5b2b4d4cd2781e3481b23fe9a12a4f3d84026e9f
jumoon9617 <kazma_mole@icloud.com>
2024-05-15 00:06:36 +0900

    add music_list

--- musicApp.py
+++ musicApp.py
@@ -3,33 +3,58 @@ import os
 import pygame as pg
 from tkinter import filedialog
 
-pg.init()
+selected_file=None
 
+#pygameの初期化
+pg.init()
+pg.mixer.init()
+
+#mp3ファイルのリスト
+def loadMusicFiles():
+    music_folder = "music_files"
+    music_files = [f for f in os.listdir(music_folder) if f.endswith('.mp3')]
+    return music_files
+
+#音楽選択メソッド
+def selectMusic():
+    global selected_file
+    selected_file = filedialog.askopenfilename(initialdir="music_files", title="Select Music File", filetypes=(("MP3 files", "*.mp3"), ("All files", "*.*")))
+    #print("Selected music:", selected_file)
+
+#音楽再生メソッド
 def playMusic():
     music_folder = "music_files"
-    music_file = "ゆきうさだるま.mp3"
-    folder_path = os.path.abspath(music_folder)
-    file_path = os.path.join(folder_path, music_file)
-
+    file_path = os.path.join(music_folder, selected_file)
     pg.mixer.music.load(file_path)
     pg.mixer.music.play()
-    
     
     # while pg.mixer.music.get_busy():
     #     pg.time.Clock().tick(10)
     
     # pg.quit()
 
+#再生停止
 def stopMusic():
     pg.mixer.music.stop()
 
 #GUI
 root = tk.Tk()
-root.geometry("300x300")
+root.geometry("400x300")
+root.title("Music Player")
 
-# selectボタン
-#selectButton = tk.Button(root, text="Select Music", command=selectMusic)
-#selectButton.pack(pady=10)
+def onSelect(event):
+    global selected_file
+    selected_index = listbox.curselection()
+    if selected_index:
+        selected_file = listbox.get(selected_index[0])
+
+#音楽リスト
+music_files = loadMusicFiles()
+listbox = tk.Listbox(root, selectmode=tk.SINGLE)
+for file in music_files:
+    listbox.insert(tk.END, file)
+listbox.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+listbox.bind('<<ListboxSelect>>', onSelect)
 
 # playボタン
 playButton = tk.Button(root, text="Play Music", command=playMusic)
@@ -39,4 +64,8 @@ playButton.pack(pady=10)
 stopButton = tk.Button(root, text="Stop Music", command=stopMusic)
 stopButton.pack(pady=10)
 
+#selectイベント
+listbox.bind("<<ListboxSelect>>", onSelect)
+
+#メインループ
 root.mainloop()
